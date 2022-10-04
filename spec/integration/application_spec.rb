3e41a147b8267b3df3fe7096025b51b21efc18ac
kateusacova <kateusacova@gmail.com>
2022-10-04 11:15:00 +0100

    Two exercises completed

---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds/albums_seeds.sql')
+  connection = PG.connect({ host: ENV['HOST'], dbname: 'music_library_test', user: 'postgres', password: ENV['PASSWORD'] })
+  connection.exec(seed_sql)
+end
+
+RSpec.describe Application do
+  include Rack::Test::Methods
+
+  before(:each) do
+    reset_albums_table
+  end
+  
+  let(:app) { Application.new }
+
+  context "GET to /albums" do
+    it "Returns 200 OK with list of albums" do
+      response = get("/albums")
+
+      expected_response = "Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context "POST to /albums" do
+    it "Returns 200 OK and created new album" do
+      response = post("/albums", title: "Voyage", release_year: 2022, artist_id: 2)
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get("/albums")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('Voyage')
+    end
+  end
+
+end
